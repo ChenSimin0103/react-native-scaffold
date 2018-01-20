bb062609b48c45923388dcd0e68d9041cffa2427
Chen Simin <375021462@qq.com>
2018-01-20 17:51:43 +0800

    完成配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+
+  'rules': {
+//    // allow paren-less arrow functions
+//    'arrow-parens': 0,
+//    // allow async-await
+//    'generator-star-spacing': 0,
+//    // allow debugger during development
+//    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+//    'brace-style': ['error', 'allman', { allowSingleLine: true }],
+    'linebreak-style': 0
+  }
 };
